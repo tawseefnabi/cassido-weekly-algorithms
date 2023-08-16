9af4218bf210a67a15aeb24aeaa2cd62f481561e
tawseefnabi9 <tawseef_bhat@discovery.com>
2023-08-16 08:59:29 +0530

    faultyKeeb-aug-14-assignment

---
+++ faultyKeeb.js
@@ -0,0 +1,31 @@
+// This week's (Aug 14 2023 question:
+// You have a faulty keyboard. Whenever you type a vowel on it (a,e,i,o,u,y), it reverses the string that you have written, instead of typing the character. Typing other characters works as expected. Given a string, return what will be on the screen after typing with your faulty keyboard.
+
+// Example:
+
+// > faultyKeeb('string')
+// > 'rtsng'
+
+// > faultyKeeb('hello world!')
+// > 'w hllrld!'
+
+const faultyKeeb = (word) => {
+    var faultyString = ""
+    for (let chr of word){
+      if("aeiou".includes(chr)){
+        faultyString =  reverseString(faultyString)
+        
+      } else{
+        faultyString +=chr
+      }
+    }
+    return faultyString
+}
+function reverseString(str){
+  return str.split("").reverse().join("")
+}
+
+console.log(faultyKeeb('hello world!'))
+console.log(faultyKeeb('string'))
+console.log(faultyKeeb('programming'))
+console.log(faultyKeeb('cassidoo'))
