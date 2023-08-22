713cbdc8bfe87b4884bfaa7f4cb923cc5f2fb059
tawseefnabi9 <tawseef_bhat@discovery.com>
2023-08-22 11:23:59 +0530

    guess number

---
+++ guessNumber.js
@@ -0,0 +1,26 @@
+// Make a "guessing game" where there is a target number, 
+// and as the user makes guesses, the output returns higher or lower until the user is correct.
+const prompt = require("prompt-sync")()
+
+guessNumber = () =>{
+  let maxGuessess = 10
+  let guessCount = 0
+  let guess = Math.floor(Math.random()*100) +1
+  // take input from the user
+  let num = prompt("Guess number between 1 to 100: ")
+  while(guessCount<maxGuessess ){
+    // guess = Math.floor(Math.random()*100) +1
+      if(num>guess){
+        console.log(`Your ${num} is too high, Please enter lower and your guess count is ${guessCount}`);
+        guessCount++
+        num = prompt("Guess number between 1 to 100: ")
+      } else if(num <guess){
+        console.log(`Your ${num} is too low , Please enter lower and your guess count is ${guessCount}`);
+        guessCount++ 
+        num = prompt("Guess number between 1 to 100: ")
+      } 
+  } 
+  console.log(`Sorry you have exceeded all ${maxGuessess} guessess. Guess numbers is  ${guess}`);  
+  
+}
+guessNumber();
