552e0f5c859f864e1cb295feea49f7208fba2f87
tawseefnabi <tawseefnabi9@gmail.com>
2023-10-10 12:06:27 +0530

    parse string to num

--- string_to_numbers.ts
+++ string_to_numbers.ts
@@ -8,46 +8,64 @@
 // > 123
 
 // https://buttondown.email/cassidoo/archive/all-great-achievements-require-time-maya-angelou/
-
+const wordToNumMap: Record<string, number> = {
+  zero: 0,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+  ten: 10,
+  eleven: 11,
+  twelve: 12,
+  thirteen: 13,
+  fourteen: 14,
+  fifteen: 15,
+  sixteen: 16,
+  seventeen: 17,
+  eighteen: 18,
+  nineteen: 19,
+  twenty: 20,
+  thirty: 30,
+  forty: 40,
+  fifty: 50,
+  sixty: 60,
+  seventy: 70,
+  eighty: 80,
+  ninety: 90,
+  hundred: 100,
+  thousand: 1000,
+  million: 1000000,
+}
 function parseNumber(englishWords:string): number {
-  const wordToNumMap: Record<string, number> = {
-    zero: 0,
-    One: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9,
-    ten: 10,
-    eleven: 11,
-    twelve: 12,
-    thirteen: 13,
-    fourteen: 14,
-    fifteen: 15,
-    sixteen: 16,
-    seventeen: 17,
-    eighteen: 18,
-    nineteen: 19,
-    twenty: 20,
-    thirty: 30,
-    forty: 40,
-    fifty: 50,
-    sixty: 60,
-    seventy: 70,
-    eighty: 80,
-    ninety: 90,
-    hundred: 100,
-    thousand: 1000,
-    million: 1000000,
-  }
+  const words = englishWords.toLowerCase().split(' ');
+  let result = 0;
+  let currentNumber = 0;
 
-  return 1
+  for (const word of words){
+    const number = wordToNumMap[word]
+    if (number === undefined) {
+      return NaN; // Invalid input
+    }
+    if(number == 100) {
+      currentNumber *= 100
+    } else if(number === 1000 || number === 1000000){
+      currentNumber *= number;
+      result += currentNumber
+      currentNumber = 0
+    } else{
+      currentNumber +=number
+    }
+  }
+  result += currentNumber;
+  return result;
 }
 
-const englishWords: string = "one hundred twenty-five thousand seven hundred sixty-four";
+const englishWords: string = "one hundred twenty thousand seven hundred sixty";
 const numericValue: number = parseNumber(englishWords);
 console.log(numericValue); // Output: 125764
 
